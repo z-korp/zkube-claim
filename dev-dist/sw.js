60cb2371f3404d5a96c41ce202d104e0d9365572
Cosmos 612 <cosmos.dev612@gmail.com>
2024-11-15 15:17:17 +0100

    AIrdrop new style

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-d95113be'], (function (workbox) { 'use strict';
+define(['./workbox-56016e7a'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-d95113be'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0k0a4gquqeo"
+    "revision": "0.0idc0nmlsuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/ui/screens/Loading.tsx
+++ src/ui/screens/Loading.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/ui/elements/button";
 import ImageAssets from "@/ui/theme/ImageAssets";
 import { useTheme } from "@/ui/elements/theme-provider/hooks";
+import { Boxes } from "../components/BackgroundBoxes";
+import { cn } from "@/lib/utils";
+import { BackgroundGradient } from "../components/BackgroundGradient";
+import ConfettiExplosion from "../components/ConfettiExplosion";
+import { useRef } from "react";
 
 export const Loading = ({
   enter,
@@ -13,32 +18,30 @@ export const Loading = ({
   const imgAssets = ImageAssets(themeTemplate);
 
   return (
-    <div className="h-screen-viewport flex flex-col w-full justify-center items-center relative">
-      {/* Background */}
-      <div className="absolute inset-0 overflow-hidden z-10">
-        <div
-          className="absolute inset-0 bg-cover bg-center animate-zoom-in-out"
-          style={{ backgroundImage: `url('${imgAssets.background}')` }}
-        />
-      </div>
+    <div className="h-screen-viewport relative overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg gap-8">
+      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
-      {/* Logo and Enter Button */}
-      <div className="flex flex-col justify-center items-center w-full h-full z-30">
-        <img
-          src={imgAssets.logo}
-          alt="logo"
-          className={`h-32 md:h-40 ${enter && "animate-load"}`}
-        />
-        {!enter && (
-          <Button
-            onClick={() => setEnter(true)}
-            className="text-2xl mt-8"
-            variant="default"
-          >
-            Enter
-          </Button>
-        )}
+      <Boxes />
+
+      <h1 className="md:text-8xl text-4xl text-white my-10 z-20">
+        zKube AirDrop
+      </h1>
+      <img
+        src={imgAssets.logo}
+        alt="logo"
+        className={`h-32 md:h-40 my-10 z-20 ${enter && "animate-load"}`}
+      />
+
+      <div className="my-10 z-20">
+        <BackgroundGradient className="bg-white bg-slate-900">
+          <button className="p-4 text-4xl" onClick={() => setEnter(true)}>
+            <ConfettiExplosion enter={enter}>
+              {enter ? "Loading ..." : "Enter"}{" "}
+            </ConfettiExplosion>
+          </button>
+        </BackgroundGradient>
       </div>
+      {/* Logo and Enter Button */}
     </div>
   );
 };
