66df9b8ea577041f290c7455b04cf6e45012343d
Cosmos 612 <cosmos.dev612@gmail.com>
2024-11-20 13:27:12 +0100

    confetti on boxes

--- src/ui/screens/Home.tsx
+++ src/ui/screens/Home.tsx
@@ -28,45 +28,45 @@ export const HomePage = () => {
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
       <ConfettiExplosion enter={false}>
         <Boxes />
-
-        <div className="flex flex-col gap-8 justify-center items-center min-h-screen">
-          <h1 className="text-8xl mb-8 text-neon animate-neon z-20">zKube</h1>
-          <div
-            className={`flex justify-center w-[95%] items-center ${isMdOrLarger ? "px-24" : ""}`}
-          >
-            {isMdOrLarger && (
-              <div className={`relative animate-load z-20`}>
-                <img
-                  src={imgAssets.logo}
-                  alt="logo"
-                  className={`h-32 md:h-40 z-20 relative`}
-                />
-                <div className="absolute inset-0 rounded-full bg-[#66D4F6] blur-xl opacity-60 animate-pulseOpacity z-10"></div>
-              </div>
-            )}
-            <Airdrop />
-            {isMdOrLarger && (
-              <div className={`relative animate-load z-20`}>
-                <img
-                  src={imgAssets.logo}
-                  alt="logo"
-                  className={`h-32 md:h-40 z-20 relative`}
-                />
-                <div className="absolute inset-0 rounded-full bg-[#66D4F6] blur-xl opacity-60 animate-pulseOpacity z-10"></div>
-              </div>
-            )}
-          </div>
-          <br></br>
-          <div className={`relative animate-load z-20`}>
-            <img
-              src={imgAssets.logo}
-              alt="logo"
-              className={`h-32 md:h-40 z-20 relative`}
-            />
-            <div className="absolute inset-0 rounded-full bg-[#66D4F6] blur-xl opacity-60 animate-pulseOpacity z-10"></div>
-          </div>
-        </div>
       </ConfettiExplosion>
+
+      <div className="flex flex-col gap-8 justify-center items-center min-h-screen">
+        <h1 className="text-8xl mb-8 text-neon animate-neon z-20">zKube</h1>
+        <div
+          className={`flex justify-center w-[95%] items-center ${isMdOrLarger ? "px-24" : ""}`}
+        >
+          {isMdOrLarger && (
+            <div className={`relative animate-load z-20`}>
+              <img
+                src={imgAssets.logo}
+                alt="logo"
+                className={`h-32 md:h-40 z-20 relative`}
+              />
+              <div className="absolute inset-0 rounded-full bg-[#66D4F6] blur-xl opacity-60 animate-pulseOpacity z-10"></div>
+            </div>
+          )}
+          <Airdrop />
+          {isMdOrLarger && (
+            <div className={`relative animate-load z-20`}>
+              <img
+                src={imgAssets.logo}
+                alt="logo"
+                className={`h-32 md:h-40 z-20 relative`}
+              />
+              <div className="absolute inset-0 rounded-full bg-[#66D4F6] blur-xl opacity-60 animate-pulseOpacity z-10"></div>
+            </div>
+          )}
+        </div>
+        <br></br>
+        <div className={`relative animate-load z-20`}>
+          <img
+            src={imgAssets.logo}
+            alt="logo"
+            className={`h-32 md:h-40 z-20 relative`}
+          />
+          <div className="absolute inset-0 rounded-full bg-[#66D4F6] blur-xl opacity-60 animate-pulseOpacity z-10"></div>
+        </div>
+      </div>
 
       {/* Content Layer */}
     </div>
